110aa7be462a85ef8e08927c521803dbb74a09d7
Calebm5577 <calebm5577@gmail.com>
2020-07-05 15:52:12 -0400

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,77 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, { useState, useEffect } from "react";
+import ReactDOM from "react-dom";
+import axios from "axios";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
+function App() {
+  const [countries, setCountries] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [search, setSearch] = useState("");
+  const [filteredCountries, setFilteredCountries] = useState([]);
+
+  useEffect(() => {
+    setLoading(true);
+    axios
+      .get("https://restcountries.eu/rest/v2/all")
+      .then(res => {
+        setCountries(res.data);
+        setLoading(false);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }, []);
+
+  useEffect(() => {
+    setFilteredCountries(
+      countries.filter(country =>
+        country.name.toLowerCase().includes(search.toLowerCase())
+      )
+    );
+  }, [search, countries]);
+
+  if (loading) {
+    return <p>Loading countries...</p>;
+  }
+
+  return (
+    <div className="App">
+      <h1>Countries Lists</h1>
+      <p>
+        If you like my work, please support by subscribing{" "}
+        <a
+          style={{ color: "red" }}
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://www.youtube.com/channel/UCdItDI6oTgPW7l9WOJI7ItA/"
+        >
+          D'coders youtube channel
+        </a>
+      </p>
+      <input
+        type="text"
+        placeholder="Search Countries"
+        onChange={e => setSearch(e.target.value)}
+      />
+      {filteredCountries.map((country, idx) => (
+        <CountryDetail key={idx} {...country} />
+      ))}
+    </div>
+  );
+}
+
+const CountryDetail = props => {
+  const { name, flag } = props;
+  return (
+    <>
+      <p>
+        <img src={flag} alt={name} style={{ width: "20px", height: "20px" }} />
+      </p>
+      <p>{name}</p>
+    </>
+  );
+};
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
