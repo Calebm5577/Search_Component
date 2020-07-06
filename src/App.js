6de5893b82aa65ca4c896635be1925c89c9ad14a
Calebm5577 <calebm5577@gmail.com>
2020-07-05 21:27:28 -0400

    Second Commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
-
+import logo from './logo.svg';
+import './App.css';
+import AutoCompleteText from './AutoCompleteText';
+import countries from './countries';
 
 function App() {
   return (
     <div className="App">
-
+      <div className="App-Component">
+        <div className="App-Component">
+          <AutoCompleteText items={countries}/>
+        </div>
+      </div>
     </div>
   );
 }
